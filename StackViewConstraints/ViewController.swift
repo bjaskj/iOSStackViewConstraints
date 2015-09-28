a60fedd6f5ed3d4a8bfa5d7184f76dae41f61cf9
Bjarte Skj√∏restad <bjarte@timeanddate.com>
2015-09-28 10:33:53 +0200

    Initial Commit

---
+++ StackViewConstraints/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackViewConstraints
+//
+//  Created by Bjarte Skjørestad on 28/09/15.
+//  Copyright © 2015 Time and Date AS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
